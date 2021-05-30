88e0ccc33ceabefa6e089b6ba2fc21c05e202c6e
chrisjc9931 <>
2021-05-30 05:22:27 +0000

    added loops studio b

--- index.js
+++ index.js
@@ -164,3 +164,18 @@ if (jj < numbers3333.length) {
 /* On the other hand, the while loop is more flexible, as we saw with the example of validating user input. In that case, we could not determine in advance how many times the loop would iterate; it depended entirely on the values provided by the user during program execution. For this reason, a while loop is often described as indefinite iteration. We expect that eventually the condition controlling the iteration will evaluate to false and the iteration will stop.
 
 (Unless we have an infinite loop, which is a problem we want to avoid.) While we saw that any for loop can be written as a while loop by manually creating and updating a loop variable, it is preferable to use a for loop when iterating over a collection or iterating a fixed number of times. Manually updating the variable in a while loop is more work for you, the programmer, and can lead to infinite loops if not handled properly */
+
+let  str1 = 'Rutabagas';
+let  str2 = 'Chemistry';
+let str3 = ""
+for (let i = 0; i < str1.length; i++){
+  str3 += `${str1[i]}${str2[i]}`.toLowerCase();
+}
+
+console.log(str3);
+
+let str4 = []; // I wrote this example to use push instead of the string literal
+for(let i = 0; i < str1.length; i++){
+  str4.push(str1[i],str2[i]);
+}
+console.log(str4.join("").toUpperCase());
